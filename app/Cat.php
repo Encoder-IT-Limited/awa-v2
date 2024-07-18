6ea967441067d2a2b4322a84aae5959c111152ea
nahid <nahidferdous240@gmail.com>
2024-07-18 11:51:22 +0600

    Update to master

--- app/Cat.php
+++ app/Cat.php
@@ -3,23 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 class Cat extends Model
 {
 
-  use LogsActivity;
+    use LogsActivity, SoftDeletes;
 
-  protected $table = 'cats';
-  protected $primaryKey = 'id';
+    protected $table = 'cats';
+    protected $primaryKey = 'id';
 
-  protected $fillable = ['name', 'code', 'count', 'text', 'ort', 'hints', 'beschreibung', 'words', 'extra', 'referenz', 'group', 'stat', 'deleted_at'];
+    protected $fillable = ['name', 'code', 'count', 'text', 'ort', 'hints', 'beschreibung', 'fulldescription', 'words', 'extra', 'referenz', 'group', 'stat', 'deleted_at'];
 
-  protected static $logAttributes = ['name', 'code', 'count', 'text', 'ort', 'hints', 'beschreibung', 'words', 'extra', 'referenz', 'group', 'stat', 'deleted_at'];
+    protected static $logAttributes = ['name', 'code', 'count', 'text', 'ort', 'hints', 'beschreibung', 'words', 'extra', 'referenz', 'group', 'stat', 'deleted_at'];
 
-  public function project() {
+    public function project()
+    {
 
-    return $this->hasMany('App\Project');
-  }
+        return $this->hasMany('App\Project');
+    }
 
 }
